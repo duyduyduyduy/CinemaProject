3e8869e0bb4adee2176d25d3d1fa3f35dde2edd3
vu <vu@gmail.com>
2023-02-03 20:04:02 +0700

    Update Header1: PHIM and SỰ KIỆN

--- src/Components/Header1/Header1.tsx
+++ src/Components/Header1/Header1.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { connect } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect} from "react";
 import "./Header1.scss";
+import eFilm from "../Model/eFilm";
 
-export default function Header1() {
+function Header1(props: any) {
+  const [filmModal, setFilmModal] = useState<Boolean>(false);
+  const [eventModal, setEventModal] = useState<Boolean>(false)
+  const [lsCurrentFilm, setLsCurrentFilm] = useState<Array<eFilm>>([]);
+  const [lsSoonFilm, setLsSoonFilm] = useState<Array<eFilm>>([]);
+
+
+  useEffect(() => {    
+    setLsCurrentFilm(props.CurrentFilmState.lsCurFilm);
+    setLsSoonFilm(props.NextFilmState.lsNextFilm);
+  }, [props]);
+
+  const nav = useNavigate();
+  const HandleClickBuy = (id: string) => {
+    nav("Film/" + id)
+  }
   return (
     <div className="Header">
       <div className="mainSide">
@@ -11,16 +29,84 @@ export default function Header1() {
             <li>
               <a href="#">MUA VÉ</a>
             </li>
-            <li>
-              <a href="#">PHIM</a>
+            <li className="film-li"
+              onMouseEnter={() => setFilmModal(true)}
+              onMouseLeave={() => setFilmModal(false)}
+            >
+              <a href="#"
+                // onMouseEnter={() => setFilmModal(true)}
+                // onMouseLeave={() => setFilmModal(false)}
+              >PHIM</a>
               <i className="fa-solid fa-angle-down"></i>
+              {/* FilmDropdown */}
+              {
+                filmModal && (
+                  <div className="FilmDropdown">
+                        {/* currentFilm */}
+                    <div className="currentFilm">
+                      <span>Phim đang chiếu</span>
+                      <div className="currentFilmcontainer">
+                        {lsCurrentFilm?.map((item, index) => {
+                          return (
+                            index <= 3 && (
+                              <div onClick={() => HandleClickBuy(item.id)}>
+                                <div>
+                                  <img src={item.imageLandscape} alt="something" />
+                                </div>
+                                <p>
+                                  {" "}
+                                  {item.name.length > 35
+                                    ? item.name.slice(0, 30) + "..." : item.name
+                                  }
+                                </p>
+                              </div>                    
+                            )
+                          )
+                        })}
+
+                      </div>
+                    </div>
+                        {/* upcomingFilm */}
+                    <div className="upcomingFilm currentFilm">
+                      <span>Phim đang chiếu</span>
+                      <div className="currentFilmcontainer">
+                        {lsSoonFilm?.map((item, index) => {
+                          return (
+                            index <= 3 && (
+                              <div onClick={() => HandleClickBuy(item.id)}>
+                                <div>
+                                  <img src={item.imageLandscape} alt="something" />
+                                </div>
+                                <p>
+                                  {item.name.length > 35 ? item.name.slice(0, 30) + "...": item.name}
+                                </p>
+                              </div>
+                            )
+                          )
+                        })}
+                      </div>
+                    </div>
+                  </div>
+
+                )
+              }
+
             </li>
             <li>
               <a href="#">BLOG ĐIỆN ẢNH</a>
             </li>
-            <li>
+            <li className="event-li"
+              onMouseEnter={() => setEventModal(true)}
+              onMouseLeave={() => setEventModal(false)}
+            >
               <a href="#">SỰ KIỆN</a>
               <i className="fa-solid fa-angle-down"></i>
+              {eventModal && (
+                <div className="event">
+                  <p>Ưu đãi</p>
+                  <p>Phim hay tháng</p>
+                </div>
+              )}
             </li>
           </ul>
         </div>
@@ -28,5 +114,14 @@ export default function Header1() {
         <div className="sign"></div>
       </div>
     </div>
-  );
+  )
 }
+
+const mapStateToProps = (state: any, ownProps: any) => {
+  return {
+    CurrentFilmState: state.CurrentFilmState,
+    NextFilmState: state.NextFilmState
+  }
+}
+
+export default connect(mapStateToProps, null)(Header1)
