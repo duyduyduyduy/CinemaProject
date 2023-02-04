cd3f5284761eff149f9f38498c6c7ddddad514c1
Duy Bùi Thanh <btduy12.11lvl@gmail.com>
2023-02-04 23:49:28 +0700

    Update Login with cookies

--- src/Components/Header1/Header1.tsx
+++ src/Components/Header1/Header1.tsx
@@ -1,103 +1,120 @@
 import { connect } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./Header1.scss";
 import eFilm from "../Model/eFilm";
-
+import Cookies from "js-cookie";
 function Header1(props: any) {
   const [filmModal, setFilmModal] = useState<Boolean>(false);
-  const [eventModal, setEventModal] = useState<Boolean>(false)
-  const [signModal, setSignModal] = useState<Boolean>(false)
+  const [eventModal, setEventModal] = useState<Boolean>(false);
+  const [signModal, setSignModal] = useState<Boolean>(false);
 
   const [lsCurrentFilm, setLsCurrentFilm] = useState<Array<eFilm>>([]);
   const [lsSoonFilm, setLsSoonFilm] = useState<Array<eFilm>>([]);
 
-
-  useEffect(() => {    
+  useEffect(() => {
     setLsCurrentFilm(props.CurrentFilmState.lsCurFilm);
     setLsSoonFilm(props.NextFilmState.lsNextFilm);
   }, [props]);
-
+  const handleOnClickSignOut = () => {
+    Cookies.remove("Email");
+    Cookies.remove("Password");
+    Cookies.remove("Name");
+    props.unLogin();
+    console.log("Log out");
+  };
   const nav = useNavigate();
   const HandleClickBuy = (id: string) => {
-    nav("Film/" + id)
-  }
+    nav("Film/" + id);
+  };
   return (
     <div className="Header">
       <div className="mainSide">
-        <img src={require("./Header1IMG/logo4.png")} alt="Anh Vũ" onClick={() => nav("/")}/>
+        <img
+          src={require("./Header1IMG/logo4.png")}
+          alt="Anh Vũ"
+          onClick={() => nav("/")}
+        />
         <div className="menu">
           <ul>
             <li>
               <a href="#">MUA VÉ</a>
             </li>
-            <li className="film-li"
+            <li
+              className="film-li"
               onMouseEnter={() => setFilmModal(true)}
               onMouseLeave={() => setFilmModal(false)}
             >
-              <a href="#"
+              <a
+                href="#"
                 // onMouseEnter={() => setFilmModal(true)}
                 // onMouseLeave={() => setFilmModal(false)}
-              >PHIM</a>
+              >
+                PHIM
+              </a>
               <i className="fa-solid fa-angle-down"></i>
               {/* FilmDropdown */}
-              {
-                filmModal && (
-                  <div className="FilmDropdown">
-                        {/* currentFilm */}
-                    <div className="currentFilm">
-                      <span>Phim đang chiếu</span>
-                      <div className="currentFilmcontainer">
-                        {lsCurrentFilm?.map((item, index) => {
-                          return (
-                            index <= 3 && (
-                              <div onClick={() => HandleClickBuy(item.id)}>
-                                <div>
-                                  <img src={item.imageLandscape} alt="something" />
-                                </div>
-                                <p>
-                                  {" "}
-                                  {item.name.length > 35
-                                    ? item.name.slice(0, 30) + "..." : item.name
-                                  }
-                                </p>
-                              </div>                    
-                            )
-                          )
-                        })}
-
-                      </div>
-                    </div>
-                        {/* upcomingFilm */}
-                    <div className="upcomingFilm currentFilm">
-                      <span>Phim đang chiếu</span>
-                      <div className="currentFilmcontainer">
-                        {lsSoonFilm?.map((item, index) => {
-                          return (
-                            index <= 3 && (
-                              <div onClick={() => HandleClickBuy(item.id)}>
-                                <div>
-                                  <img src={item.imageLandscape} alt="something" />
-                                </div>
-                                <p>
-                                  {item.name.length > 35 ? item.name.slice(0, 30) + "...": item.name}
-                                </p>
+              {filmModal && (
+                <div className="FilmDropdown">
+                  {/* currentFilm */}
+                  <div className="currentFilm">
+                    <span>Phim đang chiếu</span>
+                    <div className="currentFilmcontainer">
+                      {lsCurrentFilm?.map((item, index) => {
+                        return (
+                          index <= 3 && (
+                            <div onClick={() => HandleClickBuy(item.id)}>
+                              <div>
+                                <img
+                                  src={item.imageLandscape}
+                                  alt="something"
+                                />
                               </div>
-                            )
+                              <p>
+                                {" "}
+                                {item.name.length > 35
+                                  ? item.name.slice(0, 30) + "..."
+                                  : item.name}
+                              </p>
+                            </div>
                           )
-                        })}
-                      </div>
+                        );
+                      })}
                     </div>
                   </div>
-
-                )
-              }
-
+                  {/* upcomingFilm */}
+                  <div className="upcomingFilm currentFilm">
+                    <span>Phim đang chiếu</span>
+                    <div className="currentFilmcontainer">
+                      {lsSoonFilm?.map((item, index) => {
+                        return (
+                          index <= 3 && (
+                            <div onClick={() => HandleClickBuy(item.id)}>
+                              <div>
+                                <img
+                                  src={item.imageLandscape}
+                                  alt="something"
+                                />
+                              </div>
+                              <p>
+                                {item.name.length > 35
+                                  ? item.name.slice(0, 30) + "..."
+                                  : item.name}
+                              </p>
+                            </div>
+                          )
+                        );
+                      })}
+                    </div>
+                  </div>
+                </div>
+              )}
             </li>
             <li>
               <a href="#">BLOG ĐIỆN ẢNH</a>
             </li>
-            <li className="event-li"
+            <li
+              className="event-li"
               onMouseEnter={() => setEventModal(true)}
               onMouseLeave={() => setEventModal(false)}
             >
@@ -112,37 +129,79 @@ function Header1(props: any) {
             </li>
           </ul>
         </div>
-
-        <div className="sign"
-          // onMouseEnter={() => setSignModal(true)}
-          onMouseLeave={() => setSignModal(false)}
-        >
-          <div>
-            <i className="fa-solid fa-user" style={{fontSize: "20px", color: "red"}}></i>
-            <p  onMouseEnter={() => setSignModal(true)}>Hoàng Anh Vũ</p>
-            <i className="fa-solid fa-angle-down" style={{fontSize: '15px', color: 'rgb(97, 96, 96)'}}></i>
-          </div>
-          {signModal && (
-            <div className="popup-user">
-              <div>
-                <h3>Hoàng Anh Vũ</h3>
-                <p>UserCinema@Prd04.com</p>
-              </div>
-              <h2>Edit Profile</h2>
-              <h2 className="clear-h2">Sign Out</h2>
+        {props.ModalPopupState.login ? (
+          <div
+            className="sign"
+            // onMouseEnter={() => setSignModal(true)}
+            onMouseLeave={() => setSignModal(false)}
+          >
+            <div>
+              <i
+                className="fa-solid fa-user"
+                style={{ fontSize: "20px", color: "red" }}
+              ></i>
+              <p
+                onMouseEnter={() => setSignModal(true)}
+                style={{ cursor: "pointer" }}
+              >
+                {Cookies.get("Name")}
+              </p>
+              <i
+                className="fa-solid fa-angle-down"
+                style={{ fontSize: "15px", color: "rgb(97, 96, 96)" }}
+              ></i>
             </div>
-          )}
-        </div>
+            {signModal && (
+              <div className="popup-user">
+                <div>
+                  <h3> {Cookies.get("Name")}</h3>
+                  <p>{Cookies.get("Email")}</p>
+                </div>
+                <h2 style={{ cursor: "pointer" }}>
+                  {" "}
+                  <i className="fa-solid fa-pen-to-square"></i> Edit Profile
+                </h2>
+                <h2
+                  className="clear-h2"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => handleOnClickSignOut()}
+                >
+                  <i className="fa-solid fa-right-from-bracket"></i> Sign Out
+                </h2>
+              </div>
+            )}
+          </div>
+        ) : (
+          <div className="loginsignup">
+            <span className="SignIn" onClick={() => props.openSignIn()}>
+              Sign in
+            </span>
+          </div>
+        )}
       </div>
     </div>
-  )
+  );
 }
 
 const mapStateToProps = (state: any, ownProps: any) => {
   return {
     CurrentFilmState: state.CurrentFilmState,
-    NextFilmState: state.NextFilmState
-  }
-}
-
-export default connect(mapStateToProps, null)(Header1)
+    NextFilmState: state.NextFilmState,
+    ModalPopupState: state.ModalPopupState,
+  };
+};
+const mapDispatchToProps = (dispatch: any, ownProps: any) => {
+  return {
+    openSignIn: () => {
+      dispatch({
+        type: "OPEN_SIGN_IN_MODAL",
+      });
+    },
+    unLogin: () => {
+      dispatch({
+        type: "UN_LOGIN",
+      });
+    },
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Header1);
