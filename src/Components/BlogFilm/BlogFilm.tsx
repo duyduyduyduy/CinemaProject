39b0659d24fcd0b30e61109f9f98f2b3de2b8295
vu <vu@gmail.com>
2023-02-06 12:45:35 +0700

    Update: BlogFilm - ListBlogFeatured

--- src/Components/BlogFilm/BlogFilm.tsx
+++ src/Components/BlogFilm/BlogFilm.tsx
@@ -1,8 +1,21 @@
 // import { url } from 'inspector'
-import React from 'react'
+import { log } from 'console'
+import React, { useEffect, useState } from 'react'
+import eBlog from '../Model/eBlog'
 import './BlogFilm.scss'
 
 export default function BlogFilm() {
+    const [blogData, setBlogData] = useState<Array<eBlog>>([])
+
+    useEffect(() => {
+        fetch('https://mocki.io/v1/9f78daae-b0b3-4e80-90e0-ff3472bbb055?fbclid=IwAR0P010WN0LeqCPU8cHHTXH3GadDuW3vt6cj_wH8XNT_ICc3TVWsZ_WcaDI')
+            .then(res => res.json())
+            .then((data) => 
+                setBlogData(data.pageProps.dataBlog.Data.ListBlogFeatured.Items)
+            );
+    }, [])
+    console.log(blogData);
+
   return (
     <div className='BlogFilm'>
         <div className='mainSize'>
@@ -12,33 +25,49 @@ export default function BlogFilm() {
                     luôn được cập nhật tại đây!</p>
             </div>
 
+            
             <div className="ListBlogFeatured">
                 <div className='top'>
-                    <div style={{backgroundImage: `url("https://static.mservice.io/blogscontents/s770x370/momo-upload-api-220131203141-637792579015243080.jpg")`}}>
-                        <div className='viewPoint'>
-                            <span className='viewCont'>
-                                <i className="fa-sharp fa-solid fa-eye"></i>
-                                <span>182.2K</span>
-                            </span>
-                        </div>
-                        <div className='content'>
-
-                        </div>
-                    </div>
-
-                    <div style={{backgroundImage: `url("https://static.mservice.io/blogscontents/s770x370/momo-upload-api-220131203141-637792579015243080.jpg")`}}>
-
-                    </div>
-
-
-
+                    {blogData?.map((item, index) => {
+                        return (
+                            index <= 1 && (
+                                <div style={{backgroundImage: `url(${item.Avatar})`}}>
+                                    <div className='viewPoint'>
+                                        <span className='viewCont'>
+                                            <i className="fa-sharp fa-solid fa-eye"></i>
+                                            <span>{item.TotalViewsFormat}</span>
+                                        </span>
+                                    </div>
+                                    <div className='content'>
+                                        <h1>{item.Title}</h1>
+                                        <p>{item.ShortContent}</p>
+                                    </div>
+                                </div>
+                            )
+                        )
+                    })}
                 </div>
-
                 <div className='bottom'>
-
+                    {blogData?.map((item, index) => {
+                        return (
+                            index >=1 && (
+                                <div style={{backgroundSize: 'cover', backgroundPosition: 'center',backgroundImage: `url(${item.Avatar})`}}>
+                                    <div className='viewPointBot'>
+                                        <span className='viewCont'>
+                                            <i className="fa-sharp fa-solid fa-eye"></i>
+                                            <span>{item.TotalViewsFormat}</span>
+                                        </span>
+                                    </div>
+                                    <div className='content'>
+                                        <h1>{item.Title}</h1>
+                                        <p>{item.ShortContent}</p>
+                                    </div>
+                                </div>
+                            )
+                        )
+                    })}
                 </div>
             </div>
-
 
         </div>
     </div>
