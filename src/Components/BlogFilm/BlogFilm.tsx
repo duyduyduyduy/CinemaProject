8734f027de59a9f4a4d73844db606d062f80950f
vu <vu@gmail.com>
2023-02-07 15:24:36 +0700

    Update: Complete BlogFilm:                     - FilmReContainer                     - ListBlogTopView

--- src/Components/BlogFilm/BlogFilm.tsx
+++ src/Components/BlogFilm/BlogFilm.tsx
@@ -1,15 +1,39 @@
+import { connect } from 'react-redux'
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
 import eBlog from '../Model/eBlog'
 import eFilm from '../Model/eFilm'
 import eListBlogs from '../Model/eListBlogs'
 import eListBlogTopView from '../Model/eListBlogTopView'
 import './BlogFilm.scss'
+import { useNavigate } from 'react-router-dom'
 
-export default function BlogFilm(props: any) {
+function BlogFilm(props: any) {
     const [blogData, setBlogData] = useState<Array<eBlog>>([])
     const [listBlogs, setListBlogs] = useState<Array<eListBlogs>>([])
     const [listBlogTopView, setListBlogTopView] = useState<Array<eListBlogTopView>>([])
+
+    const [lsFilm, setLsFilm] = useState<Array<eFilm>>([])
+
+    useEffect(() => {
+        setLsFilm(props.CurrentFilmState.lsCurFilm)
+    }, [props])
+
+    const nav = useNavigate();
+    const HandleClick = (id: string) => {
+        nav("/Film/" + id);
+    }
+
+    // Handle Xem Thêm
+    const [tmp, setTmp] = useState<number>(1);
+    let a = 0, b = 0;
+    // a là số lượng render ra mỗi lần nhấn Xem thêm, phần dư còn lại render lần cuối
+
+
+    const assignVar = () => {
+        a = Math.floor(listBlogs.length / 3);
+        b = listBlogs.length - (3*Math.floor(listBlogs.length / 3));
+        return false;
+    }
     
     useEffect(() => {
         fetch('https://mocki.io/v1/9f78daae-b0b3-4e80-90e0-ff3472bbb055?fbclid=IwAR0P010WN0LeqCPU8cHHTXH3GadDuW3vt6cj_wH8XNT_ICc3TVWsZ_WcaDI')
@@ -47,7 +71,7 @@ export default function BlogFilm(props: any) {
                                     </div>
                                     <div className='content'>
                                         <h1>{item.Title}</h1>
-                                        <p>{item.ShortContent}</p>
+                                        <p>{item.ShortContent.length > 100 ? item.ShortContent.slice(0, 200) + '...' : item.ShortContent}</p>
                                     </div>
                                 </div>
                             )
@@ -67,7 +91,7 @@ export default function BlogFilm(props: any) {
                                     </div>
                                     <div className='content'>
                                         <h1>{item.Title}</h1>
-                                        <p>{item.ShortContent}</p>
+                                        <p>{item.ShortContent.length > 100 ? item.ShortContent.slice(0, 200) + '...' : item.ShortContent}</p>
                                     </div>
                                 </div>
                             )
@@ -77,27 +101,43 @@ export default function BlogFilm(props: any) {
             </div>
 
 
-
             <div className='BodyContent'>
+                {assignVar()}
                 <div className='ListBlogs'>
                     {listBlogs?.map((item, index) => {
-                        return (
-                            <div className='BlogItem'>
-                                <div>
-                                    <img src={item.Avatar} alt="Anh Vũ"/>
+                        if (index < (tmp > a ? tmp*3 + b : tmp *3)) {
+                            return (
+                                <div className='BlogItem'>
+                                    <div>
+                                        <img src={item.Avatar} alt="Anh Vũ"/>
+                                    </div>
+                                    <div className='ItemInfo'>
+                                        <h3>{item.CategoryChildName}</h3>
+                                        <h4>{item.Title}</h4>
+                                        <span className='viewCont'>
+                                            <i className="fa-sharp fa-solid fa-eye"></i>
+                                            <span>{item.TotalViewsFormat} lượt xem</span>
+                                        </span>
+                                        <p>{item.ShortContent.length > 200 ? item.ShortContent.slice(0, 150) + "..." : item.ShortContent}</p>
+                                    </div>
                                 </div>
-                                <div className='ItemInfo'>
-                                    <h3>{item.CategoryChildName}</h3>
-                                    <h4>{item.Title}</h4>
-                                    <span className='viewCont'>
-                                        <i className="fa-sharp fa-solid fa-eye"></i>
-                                        <span>{item.TotalViewsFormat} lượt xem</span>
-                                    </span>
-                                    <p>{item.ShortContent.length > 200 ? item.ShortContent.slice(0, 150) + "..." : item.ShortContent}</p>
-                                </div>
-                            </div>
-                        )
+                            )
+                        }
                     })}
+                    <div className='MoreContainer'>
+                        {tmp <= a && a !== 0 && (
+                            // <a onClick={() => setTmp(tmp + 1)}>Xem Thêm</a>
+                            <button onClick={() => setTmp(tmp + 1)} className="btn-donate">
+                                Xem Thêm
+                            </button>
+                        )}
+                        {tmp > a && (
+                            // <a onClick={() => setTmp(1)}>Thu Gọn</a>
+                            <button onClick={() => setTmp(1)} className="btn-donate">
+                                Thu Gọn
+                            </button>
+                        )}
+                    </div>
                 </div>
 
                 <div className='ListBlogTopView'>
@@ -117,8 +157,44 @@ export default function BlogFilm(props: any) {
                     </div>
                 </div>
             </div>
+            
+
+            <div className='FilmReContainer'>
+                <div className='FilmReTitle'>
+                    <h1>Top phim hay</h1>
+                </div>
+                <div className='Film1Container'>
+                    {lsFilm?.map((item, index) => {
+                        return (
+                            index <= 4 && (
+                                <div className='FilmReItem' style={{backgroundImage: `url(${item.imagePortrait})`}}>
+                                    <div>
+                                        <h2>
+                                            <a onClick={() => HandleClick(item.id)} href="#">
+                                                {item.name}
+                                            </a>
+                                        </h2>    
+                                        {/* <a href={`/Film/${item.id}#FilmBanner`}></a> */}
+                                    </div>
+                                </div>
+                            )
+                        )
+                    })}
+                </div>
+            </div>
 
         </div>
     </div>
   )
 }
+
+const mapStateToProps = (state: any) => {
+    return {
+        CurrentFilmState: state.CurrentFilmState
+    }
+}
+
+
+
+
+export default connect(mapStateToProps, null)(BlogFilm)
