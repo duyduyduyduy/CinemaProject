3f7a6f74d3472a1830b2198534b152f760ee0393
vu <vu@gmail.com>
2023-02-11 11:59:49 +0700

    Update:

---
+++ src/Components/PromotionHeader/PromoItem/PromoItem.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './PromoItem.scss'
+
+export default function PromoItem() {
+  return (
+    <div className='PromoItem'>
+      <div className="mainSize">
+        
+      </div>
+    </div>
+  )
+}
