14da530d0518f41d3e0694b5dd6c59c4feee4d75
vu <vu@gmail.com>
2023-02-14 15:21:01 +0700

    Update: ATM layout

--- src/Components/Payment/Payment.tsx
+++ src/Components/Payment/Payment.tsx
@@ -2,6 +2,8 @@ import Cookies from "js-cookie";
 import React from "react";
 import "./Payment.scss";
 export default function Payment() {
+
+
   return (
     <div className="PaymentContainer">
       <div className="title">
@@ -10,14 +12,41 @@ export default function Payment() {
       </div>
 
       <div className="contentContainer">
-        <div className="">dfadsa</div>
+        <div className="LeftHandPayment">   
+          <div className="ATMContainer" style={{
+            backgroundImage: `url(${require('./1.png')})`
+          }}>
+            <div className="NameATM">
+              <img src='https://cdn.haitrieu.com/wp-content/uploads/2022/02/Icon-TPBank.png' alt="" />
+              <h1>TP Bank</h1>
+            </div>
+
+            <div className="NumCard">
+              <h3>XXXX-XXXX-XXXX-XXXX</h3>
+              <p>2717</p>
+            </div>
+
+            <div className="UserInfo">
+              <h3>NGUYEN HOANG VU</h3>
+              <div>
+                <span className="nameDay">VALID FROM</span>
+                <span className="day">04/22</span>
+                <span className="nameDay">GOOD THRU</span>
+                <span className="day">04/28</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
         <div className="RightHandPayment">
           {" "}
           <div className="intContainer">
             <select name="Chọn Loại Thẻ">
               <option value="#">Chọn thẻ</option>
-              <option value="#">TP Bank</option>
-              <option value="#">Agribank</option>
+              <option value="0">TP Bank</option>
+              <option value="1">Agribank</option>
             </select>
           </div>
           <div className="intContainer">
@@ -43,8 +72,8 @@ export default function Payment() {
           </div>
           <div className="intContainer">
             <span>
-              (*) Bằng việc click/chạm vào THANH TOÁN, bạn đã xác nhận hiểu rõ
-              các Quy Định Giao Dịch Trực Tuyến của Galaxy Cinema.
+              (*) Bằng việc click/chạm vào <b>THANH TOÁN</b>, bạn đã xác nhận hiểu rõ
+              các <b>Quy Định Giao Dịch Trực Tuyến</b> của Galaxy Cinema.
             </span>
           </div>
           <div className="intContainer">
